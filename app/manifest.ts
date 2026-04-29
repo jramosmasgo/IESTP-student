ff1efebe55e1cc1fbf487477fa52533592570977
jpramosmasgo <ramosmasgo@gmail.com>
2026-04-28 23:46:23 -0500

    feat: prepare to pwa 2

--- app/manifest.ts
+++ app/manifest.ts
@@ -10,16 +10,41 @@ export default function manifest(): MetadataRoute.Manifest {
     display: "standalone",
     background_color: "#1B2B6B",
     theme_color: "#1B2B6B",
+    id: "/",
+    orientation: "portrait",
     icons: [
       {
         src: "/icon-192.png",
         sizes: "192x192",
         type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "/icon-192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable",
       },
       {
         src: "/icon-512.png",
         sizes: "512x512",
         type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "/icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
+    ],
+    screenshots: [
+      {
+        src: "/screenshot-mobile.png",
+        sizes: "1080x1920",
+        type: "image/png",
+        form_factor: "narrow",
+        label: "Dashboard del Estudiante",
       },
     ],
   };
